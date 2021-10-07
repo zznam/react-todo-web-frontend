c0b401fce264f3a25dc88813f6706bf1092eb196
namnv09 <nam.nguyen@teqie.io>
2021-10-07 22:46:31 +0700

    define components

---
+++ src/todos/NewTodoForm.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import './NewTodoForm.css'
+const NewTodoForm = () => {
+    const [inputValue, setInputValue] = useState('');
+    return (
+        <div className="new-todo-form">
+            <input className='new-todo-input'
+                type='text'
+                placeholder="Type your new todo here"
+                value={inputValue}
+                onChange={e => setInputValue(e.target.value)} />
+            <button className='new-todo-button' type='text'>Create Todo</button>
+
+        </div>
+    )
+}
+
+export default NewTodoForm;
