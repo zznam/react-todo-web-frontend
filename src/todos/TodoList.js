cd1bac14d3204a9cd9607569d572e962050dcf37
namnv09 <nam.nguyen@teqie.io>
2021-10-08 16:30:14 +0700

    combining selectors with Reselect

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -4,8 +4,12 @@ import TodoListItem from './TodoListItem'
 import NewTodoForm from './NewTodoForm'
 import { loadTodos, deleteTodoRequest, markTodoAsCompleted } from './thunks'
 import './TodoList.css'
-import { getTodos, getTodosLoading } from "./selectors";
-const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos }) => {
+import {
+    getTodosLoading,
+    getIncompleteTodos,
+    getCompletedTodos,
+} from "./selectors";
+const TodoList = ({ incompleteTodos = [], completedTodos = [], onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos }) => {
 
     useEffect(() => {
         startLoadingTodos();
@@ -15,15 +19,18 @@ const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, isLoading, 
     const content = (
         <div className="list-wrapper">
             <NewTodoForm />
-            {todos.map(todo => <TodoListItem key={todo.text} todo={todo} onRemovePressed={onRemovePressed} onCompletedPressed={onCompletedPressed} />)}
-
+            <h3>Incomplete Todos:</h3>
+            {incompleteTodos.map(todo => <TodoListItem key={todo.text} todo={todo} onRemovePressed={onRemovePressed} onCompletedPressed={onCompletedPressed} />)}
+            <h3>Completed Todos:</h3>
+            {completedTodos.map(todo => <TodoListItem key={todo.text} todo={todo} onRemovePressed={onRemovePressed} onCompletedPressed={onCompletedPressed} />)}
         </div>
     )
     return isLoading ? loadingMessage : content;
 }
 const mapStateToProps = state => ({
     isLoading: getTodosLoading(state),
-    todos: getTodos(state),
+    incompleteTodos: getIncompleteTodos(state),
+    completedTodos: getCompletedTodos(state),
 })
 const mapDispatchToProps = dispatch => ({
     startLoadingTodos: text => dispatch(loadTodos(text)),
