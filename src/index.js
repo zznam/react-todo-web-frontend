5fa87b6d042d7f1c1c2ad76a31e8ad4935ab086d
namnv09 <nam.nguyen@teqie.io>
2021-10-09 00:57:44 +0700

    test reducers and thunks

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import { configureStore } from './store'
 
@@ -25,7 +24,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
